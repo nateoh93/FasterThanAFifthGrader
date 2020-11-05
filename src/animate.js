c6b9209c0e777b597f607c198dacb0291019d8e2
nateoh93 <nathanoh93@gmail.com>
2020-11-04 22:10:33 -0800

    add difficulty levels

--- src/animate.js
+++ src/animate.js
@@ -1,19 +1,15 @@
 let pos = 'leftFoot';
 
 function Animate() {
-    const character = document.getElementById('char1');
     const character2 = document.getElementById('char2');
     if (pos === 'leftFoot') {
-        character.src = 'public/images/r_010.png';
-        character2.src = 'public/images/run_010.png';
+        character2.src = 'public/images/erun_010.png';
         pos = 'midFoot'; 
     } else if (pos === 'midFoot') {
-        character.src = 'public/images/r_005.png';
-        character2.src = 'public/images/run_020.png';
+        character2.src = 'public/images/erun_020.png';
         pos = 'rightFoot'; 
     } else {
-        character.src = 'public/images/r_000.png';
-        character2.src = 'public/images/run_000.png';
+        character2.src = 'public/images/erun_000.png';
         pos = 'leftFoot'; 
     }
 }
--- src/computer.js
+++ src/computer.js
@@ -1,3 +1,54 @@
+let difficulty = 25;
+let firstImg = 'public/images/drun_010.png';
+let secondImg = 'public/images/drun_020.png';
+let thirdImg = 'public/images/drun_000.png';
+let img;
+
+document.getElementById('5thgrader').onclick = () => {
+    document.getElementById('char1').src = 'public/images/drun_000.png';
+    document.getElementById('computer').innerHTML = '5th Grader';
+    difficulty = 25;
+    firstImg = 'public/images/drun_010.png';
+    secondImg = 'public/images/drun_020.png';
+    thirdImg = 'public/images/drun_000.png';
+}
+
+document.getElementById('student').onclick = () => {
+    document.getElementById('char1').src = 'public/images/brun_000.png';
+    document.getElementById('computer').innerHTML = 'Student';
+    difficulty = 35;
+    firstImg = 'public/images/brun_010.png';
+    secondImg = 'public/images/brun_020.png';
+    thirdImg = 'public/images/brun_000.png';
+}
+
+document.getElementById('programmer').onclick = () => {
+    document.getElementById('char1').src = 'public/images/run_000.png';
+    document.getElementById('computer').innerHTML = 'Programmer';
+    difficulty = 55;
+    firstImg = 'public/images/run_010.png';
+    secondImg = 'public/images/run_020.png';
+    thirdImg = 'public/images/run_000.png';
+}
+
+document.getElementById('elite').onclick = () => {
+    document.getElementById('char1').src = 'public/images/crun_000.png';
+    document.getElementById('computer').innerHTML = 'Elite';
+    difficulty = 75;
+    firstImg = 'public/images/crun_010.png';
+    secondImg = 'public/images/crun_020.png';
+    thirdImg = 'public/images/crun_000.png';
+}
+
+document.getElementById('creator').onclick = () => {
+    document.getElementById('char1').src = 'public/images/arun_000.png';
+    document.getElementById('computer').innerHTML = 'Creator';
+    difficulty = 115;
+    firstImg = 'public/images/arun_010.png';
+    secondImg = 'public/images/arun_020.png';
+    thirdImg = 'public/images/arun_000.png';
+}
+
 function compMove() {
     let char1 = document.getElementById('char1');
     let pos = 0;
@@ -6,8 +57,7 @@ function compMove() {
 
     let quote = document.querySelectorAll('span');
     let length = quote.length;
-    //need to pass in speed based on difficulty chosen
-    let speed = (5*115)/480;
+    let speed = (difficulty * 5) / 480;
     let steps = length / speed;
 
     function frames() {
@@ -15,13 +65,13 @@ function compMove() {
             clearInterval(auto);
         } else {
             if (position === 'leftFoot') {
-                char1.src = 'public/images/r_010.png';
+                char1.src = firstImg;
                 position = 'midFoot'; 
             } else if (position === 'midFoot') {
-                char1.src = 'public/images/r_005.png';
+                char1.src = secondImg;
                 position = 'rightFoot'; 
             } else {
-                char1.src = 'public/images/r_000.png';
+                char1.src = thirdImg;
                 position = 'leftFoot'; 
             };
             
