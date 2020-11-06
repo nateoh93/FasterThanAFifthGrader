b11ae2895d8a6365e42c86533d517ea13a2eeee7
nateoh93 <nathanoh93@gmail.com>
2020-11-06 15:49:15 -0800

    add music and styling

--- bundle.js
+++ bundle.js
@@ -522,13 +522,25 @@ const restartGame = document.getElementById('restart-game')
 
 startGame.addEventListener('click', _start__WEBPACK_IMPORTED_MODULE_0__.default);
 
-// window.onload = function () {
-//   document.getElementById('theme-song').play();
-// }
-
 restartGame.addEventListener('click', () => {
   window.location.reload();
 });
+
+const playBtn = document.getElementById('play');
+const audio = document.getElementById('theme-song');
+const icon = document.getElementById('icon');
+
+playBtn.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    icon.classList.remove("fa-play-circle");
+    icon.classList.add("fa-pause-circle");
+  } else {
+    audio.pause();
+    icon.classList.remove("fa-pause-circle");
+    icon.classList.add("fa-play-circle");
+  }
+})
 
 /***/ }),
 
--- src/index.js
+++ src/index.js
@@ -8,10 +8,22 @@ const restartGame = document.getElementById('restart-game')
 
 startGame.addEventListener('click', LoadGame);
 
-// window.onload = function () {
-//   document.getElementById('theme-song').play();
-// }
-
 restartGame.addEventListener('click', () => {
   window.location.reload();
 });
+
+const playBtn = document.getElementById('play');
+const audio = document.getElementById('theme-song');
+const icon = document.getElementById('icon');
+
+playBtn.addEventListener('click', () => {
+  if (audio.paused) {
+    audio.play();
+    icon.classList.remove("fa-play-circle");
+    icon.classList.add("fa-pause-circle");
+  } else {
+    audio.pause();
+    icon.classList.remove("fa-pause-circle");
+    icon.classList.add("fa-play-circle");
+  }
+})
